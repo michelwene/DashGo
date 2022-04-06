af5c7e7459c45a42fb54920d5012920d7c2c0336
Michel Wene <michelwene@hotmail.com>
2022-04-06 14:46:26 -0300

    feat: validation form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,36 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../components/Form/input";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
+const SignInFormSchema = yup.object({
+  email: yup
+    .string()
+    .email("Formato de e-mail inválido")
+    .required("E-mail obrigatório"),
+  password: yup.string().required("Senha obrigatória"),
+});
 
 export default function Signin() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: yupResolver(SignInFormSchema),
+  });
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(values);
+  };
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -12,12 +41,31 @@ export default function Signin() {
         p="8"
         borderRadius={8}
         flexDirection="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing={4}>
-          <Input name="email" label="E-mail" type="email" />
-          <Input name="password" label="Senha" type="password" />
+          <Input
+            name="email"
+            label="E-mail"
+            type="email"
+            {...register("email")}
+            error={errors.email}
+          />
+          <Input
+            name="password"
+            label="Senha"
+            type="password"
+            {...register("password")}
+            error={errors.password}
+          />
         </Stack>
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={isSubmitting}
+        >
           Entrar
         </Button>
       </Flex>
--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -12,15 +12,62 @@ import Link from "next/link";
 import { Input } from "../../components/Form/input";
 import { Header } from "../../components/Header/index";
 import Siderbar from "../../components/Sidebar/index";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type CreateUserFormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+};
+
+const CreateUserFormSchema = yup.object({
+  name: yup.string().required("Nome obrigatório"),
+  email: yup
+    .string()
+    .email("Formato de e-mail inválido")
+    .required("E-mail obrigatório"),
+  password: yup
+    .string()
+    .required("Senha obrigatória")
+    .min(6, "A senha deve conter no mínimo 6 caracteres"),
+  password_confirmation: yup
+    .string()
+    .oneOf([null, yup.ref("password")], "As senhas não conferem"),
+});
 
 export default function CreateUser() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: yupResolver(CreateUserFormSchema),
+  });
+
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (
+    values
+  ) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(values);
+  };
+
   return (
     <Box>
       <Header />
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Siderbar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
+        <Box
+          as="form"
+          flex="1"
+          borderRadius={8}
+          bg="gray.800"
+          p={["6", "8"]}
+          onSubmit={handleSubmit(handleCreateUser)}
+        >
           <Heading size="lg" fontWeight="normal">
             Criar usuário
           </Heading>
@@ -28,16 +75,35 @@ export default function CreateUser() {
 
           <VStack>
             <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-              <Input name="name" label="Nome completo" />
-              <Input type="email" name="email" label="E-mail" />
+              <Input
+                name="name"
+                label="Nome completo"
+                {...register}
+                error={errors.name}
+              />
+              <Input
+                type="email"
+                name="email"
+                label="E-mail"
+                {...register}
+                error={errors.email}
+              />
             </SimpleGrid>
 
             <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-              <Input name="password" type="password" label="Nome completo" />
+              <Input
+                name="password"
+                type="password"
+                label="Nome completo"
+                {...register}
+                error={errors.password}
+              />
               <Input
                 type="password"
                 name="password_confirmation"
                 label="Confirmação da senha"
+                {...register}
+                error={errors.password_confirmation}
               />
             </SimpleGrid>
           </VStack>
@@ -46,7 +112,9 @@ export default function CreateUser() {
               <Link href="/users" passHref>
                 <Button colorScheme="whiteAlpha">Cancelar</Button>
               </Link>
-              <Button colorScheme="pink">Salvar</Button>
+              <Button colorScheme="pink" isLoading={isSubmitting}>
+                Salvar
+              </Button>
             </HStack>
           </Flex>
         </Box>
