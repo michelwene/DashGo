1173a2208434e44a5dd42b01b536f588f0b0d18e
Michel Wene <michelwene@hotmail.com>
2022-04-18 16:20:49 -0300

    feat: conected axios

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -35,7 +35,19 @@ export function makeServer() {
       this.namespace = "api";
       this.timing = 750;
 
-      this.get("/users");
+      this.get("/users", function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        const total = schema.all("user").length;
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all("user")).users.slice(
+          pageStart,
+          pageEnd
+        );
+      });
       this.post("/users");
 
       this.namespace = "";
